e79b706e7dcb31cee5a6a71fbb6250f8794a918d
Kaua_DLG <engs-kauadelgado@camporeal.edu.br>
2026-09-08 20:26:57 -0300

    ATV2: cria rotas com parametro produto/categoria/usuario

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Esta é a página de Contato.';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Produto de ID: {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria de ID: {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuário de ID: {$id}";
+});
