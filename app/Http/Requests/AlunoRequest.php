95cb41846658a3479b6188fbc060bb8cc4bff392
Krupek-bit <engs-gabrielkrupek@camporeal.edu.br>
2026-09-08 22:22:47 -0300

    ATV 15: Crie o Request para Alunos e faca validacoes

---
+++ app/Http/Requests/AlunoRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AlunoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|unique:alunos,email',
+            'curso' => 'required|string|max:255',
+        ];
+    }
+}
