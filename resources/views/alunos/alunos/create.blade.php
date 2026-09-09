35cbc3265ccb951daeb885e5aa7cf6a89ec0a6d9
Krupek-bit <engs-gabrielkrupek@camporeal.edu.br>
2026-09-08 22:21:49 -0300

    ATV 14: Crie o formulario de cadastro de Aluno

--- resources/views/alunos/alunos/create.blade.php
+++ resources/views/alunos/alunos/create.blade.php
@@ -17,3 +17,34 @@
         <button type="submit">Salvar</button>
     </form>
 @endsection
+
+@extends('layouts.app')
+
+@section('title', 'Cadastrar Aluno')
+
+@section('content')
+    <h1>Cadastrar Aluno</h1>
+
+    @if ($errors->any())
+        <ul style="color: red;">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form method="POST" action="/alunos">
+        @csrf
+
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" value="{{ old('nome') }}"><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" value="{{ old('email') }}"><br>
+
+        <label for="curso">Curso:</label>
+        <input type="text" name="curso" id="curso" value="{{ old('curso') }}"><br>
+
+        <button type="submit">Salvar</button>
+    </form>
+@endsection
